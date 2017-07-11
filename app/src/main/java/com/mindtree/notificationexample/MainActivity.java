e28ecd5360b9f93e4c98cbbd4c1e87f411021f3c
john <john.mobiledev0403@gmail.com>
2017-07-11 17:56:48 +0530

    Initial commit

---
+++ app/src/main/java/com/mindtree/notificationexample/MainActivity.java
@@ -0,0 +1,44 @@
+package com.mindtree.notificationexample;
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.provider.Settings;
+import android.support.v7.app.NotificationCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    NotificationCompat.Builder notification;
+    private static final int UNIQUE_ID = 456;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        notification = new NotificationCompat.Builder(this);
+        notification.setAutoCancel(true);
+
+    }
+
+    public void notificationBtnClicked(View v){
+        notification.setSmallIcon(R.drawable.ic_launcher);
+        notification.setTicker("This is the ticker");
+        notification.setWhen(System.currentTimeMillis() + 2000);
+
+        notification.setContentTitle("Welcome to Android");
+        notification.setContentText("Android online traninng");
+
+        Intent intent = new Intent(this, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this,0,intent, PendingIntent.FLAG_UPDATE_CURRENT);
+        notification.setContentIntent(pendingIntent);
+
+        //trigger notifications
+        NotificationManager nm = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        nm.notify(UNIQUE_ID,notification.build());
+    }
+}
